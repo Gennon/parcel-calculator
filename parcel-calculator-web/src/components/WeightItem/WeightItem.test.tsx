e0f099eef5a8a342782a992b68739eaa4ea8a139
Kim Ruben Teigen <krt@ananki.no>
2022-11-17 09:29:27 +0100

    feat(web): Added WeightItem component

---
+++ parcel-calculator-web/src/components/WeightItem/WeightItem.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { WeightItem } from './WeightItem';
+
+test('it exists', () => {
+  const { container } = render(<WeightItem id={1} />);
+  expect(container).not.toBeNull();
+});
+
+test('Will not have values below 1', () => {
+  render(<WeightItem id={1} />);
+  const input = screen.getByRole('spinbutton');
+
+  expect(input).toHaveValue('1');
+  fireEvent.change(input, { target: { value: '-1' } });
+  fireEvent.blur(input);
+  expect(input).toHaveValue('1');
+});
+
+test('Will not have values above 10', () => {
+  render(<WeightItem id={1} />);
+  const input = screen.getByRole('spinbutton');
+
+  expect(input).toHaveValue('1');
+  fireEvent.change(input, { target: { value: '100' } });
+  fireEvent.blur(input);
+  expect(input).toHaveValue('10');
+});
---
+++ parcel-calculator-web/src/components/WeightItem/WeightItem.tsx
@@ -0,0 +1,51 @@
+import { FC } from 'react';
+import {
+  FormControl,
+  FormLabel,
+  InputGroup,
+  InputRightAddon,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+} from '@chakra-ui/react';
+
+export interface Props {
+  id: number;
+  onChange?: (value: number) => void;
+}
+
+export const WeightItem: FC<Props> = ({ id, onChange }) => {
+  const handleOnChange = (_: string, numberValue: number) => {
+    onChange?.(numberValue);
+  };
+
+  return (
+    <FormControl
+      className="WeightItem"
+      shadow={'md'}
+      maxW={'sm'}
+      pb="8"
+      px={['4', '8', '12']}
+      rounded={'md'}
+    >
+      <FormLabel fontSize={'xl'}>📦 Parcel # {id}</FormLabel>
+      <InputGroup>
+        <NumberInput
+          defaultValue={1}
+          min={1}
+          max={10}
+          onChange={handleOnChange}
+        >
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+        <InputRightAddon children="kg" />
+      </InputGroup>
+    </FormControl>
+  );
+};
