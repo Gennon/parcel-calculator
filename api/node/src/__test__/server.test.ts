170d5c3e8cf733d71867a8c3990f61a8004a9b4c
Kim Ruben Teigen <krt@ananki.no>
2022-11-19 18:23:09 +0100

    test(node-api): Added Test setup

---
+++ api/node/src/__test__/server.test.ts
@@ -0,0 +1,25 @@
+import request from 'supertest';
+import server from '../server';
+
+describe('GET /', () => {
+  it('should return 200 OK', () => {
+    return request(server).get('/').expect(200);
+  });
+});
+
+describe('GET /conf', () => {
+  it('should return 200 OK', () => {
+    return request(server).get('/conf').expect(200);
+  });
+
+  it('should return a configuration object', () => {
+    return request(server)
+      .get('/conf')
+      .expect(200)
+      .then(response => {
+        expect(response.body).toHaveProperty('baseCost');
+        expect(response.body).toHaveProperty('costPerKg');
+        expect(response.body).toHaveProperty('costPerParcel');
+      });
+  });
+});
